0c7fa82197e8d664fee4b8146ca947993c9adb44
ivanlowe <ai_zrobot@siswa.um.edu.my>
2020-12-27 15:23:05 +0800

    added assignment 2

---
+++ Individual Assignments/Low Zheng Rong_WEA150007_17152121/Assignment 2/assignment/heuristic.py
@@ -0,0 +1,11 @@
+def lows_heuristic(game, player):
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    my_moves = len(game.get_legal_moves(player))
+    opponent_moves = len(game.get_legal_moves(game.get_opponent(player)))
+
+    return my_moves*my_moves - opponent_moves
